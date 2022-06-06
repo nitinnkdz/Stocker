270bd7c6ed2e449959e1792dca7972a4a40297c4
nitinnkdz <84411305+nitinnkdz@users.noreply.github.com>
2022-06-06 14:09:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import pandas as pd
 import numpy as np
 import tweepy
 import config
-from prophet import Prophet
-from prophet.plot import plot_plotly
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 import plotly.express as px
 from urllib.request import urlopen, Request
