7518e6c1278d52c736bfc71b85b90939795d637d
nitinnkdz <84411305+nitinnkdz@users.noreply.github.com>
2022-06-08 14:28:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,18 +2,18 @@ TWITTER_CONSUMER_KEY= 'QGx4ur8D8DkrofbKbUQxQFWP9'
 TWITTER_CONSUME_SECRET='TGMbf2tQsNjF97thzQXA1IiQN0oByafFyMJyRuzMYhhPdwHMOZ'
 TWITTER_ACCESS_TOKEN ='870378415211241473-Ub6anmktZY955gRDrypIusg69rdSlTe'
 TWITTER_ACCESS_TOKEN_SECRET='tcgC8JLQkg7Bykm2uwEM9UzVIdQYi5FXV3WEXY6Qr8Yb1'       
-TWITTER_USERNAMES =[
+screenname = [
     'traderstewie',
     'the_chart_life',
     'canuck2usa',
     'sunrisetrader',
-    'tmltrader',
-    'CNBC'
-    'bespokeinvest'
-    'MarketWatch'
-    'WSJdeals'
-    'MarketRebels'
-    'KimbleCharting'
+    'TMLTrader',
+    'CNBC',
+    'bespokeinvest',
+    'MarketWatch',
+    'WSJdeals',
+    'MarketRebels',
+    'KimbleCharting',
     'IBDinvestors',
     'SJosephBurns',
-    ]
+]
